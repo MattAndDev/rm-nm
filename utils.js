2f6180d887c10a0210fe5ad74b5a990faf82d8c3
Matteo Maronati <matteo.maronati@flixbus.com>
2018-06-14 00:33:37 +0200

    Imrpove rf function in utils

--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 module.exports = {
   /**
@@ -30,16 +31,15 @@ module.exports = {
  *  @param {String} baseDir - relative path to inspect for files
  *  @param {regex} regex - a vali regular expression
  *  @param {Array} results - for iteratee results
- *  @param {Integer} iteration - index for iteratee
  *  @param {String} cwd - current search path for iteratee
  *  @return {Array} array of strings with the found files
  */
-  async getNodeDirs (baseDir, results = [], iteration = 0, cwd = baseDir) {
+  async getNodeDirs (baseDir, results = [], cwd = baseDir) {
     let contents = await fs.readdirSync(cwd)
     for (var i = 0; i < contents.length; i++) {
       let isNodeModules = contents[i] === 'node_modules'
-      let hasPackageJson = fs.existsSync(`${cwd}/package.json`)
-      let hasPackageJsonLock = fs.existsSync(`${cwd}/package-lock.json`)
+      let hasPackageJson = await fs.existsSync(`${cwd}/package.json`)
+      let hasPackageJsonLock = await fs.existsSync(`${cwd}/package-lock.json`)
       let currentPath = `${cwd}/${contents[i]}`
       if (isNodeModules) {
         results.push({
@@ -50,29 +50,34 @@ module.exports = {
       }
       let isDir = fs.statSync(currentPath).isDirectory()
       if (isDir && !isNodeModules) {
-        await this.getNodeDirs(baseDir, results, iteration + 1, currentPath)
+        await this.getNodeDirs(baseDir, results, currentPath)
       }
     }
     return results
   },
 
-  async deleteFoldersRecursive (dirs) {
-    for (var i = 0; i < dirs.length; i++) {
-      let path = dirs[i].path
-      let size = 0
-      let contents = await fs.readdirSync(path)
-      for (var y = 0; y < contents.length; y++) {
-        let currentPath = `${path}/${contents[y]}`
-        if (fs.lstatSync(currentPath).isDirectory()) {
-          await this.deleteFoldersRecursive([{path: currentPath}], size)
-        } else {
-          size = size + await fs.statSync(currentPath).size
-          fs.unlinkSync(currentPath)
-        }
+
+  /**
+  *  utils.deleteFoldersRecursive
+  *
+  *  @param {Array} dirs - ex: [{path: '', ...opts}]
+  *  @param {Integer} size - accumulator for file sizes in folder
+  *  @return {Array} containing one entry representing the size of file delted
+  */
+  async deleteFolderRecursive (dir, sizes = [],) {
+    let contents = await fs.readdirSync(dir.path)
+    for (var y = 0; y < contents.length; y++) {
+      let currentPath = path.resolve(`${dir.path}/${contents[y]}`)
+      if (!await fs.lstatSync(currentPath).isDirectory()) {
+        sizes.push(await fs.statSync(currentPath).size)
+        await fs.unlinkSync(currentPath)
+        continue
       }
-      fs.rmdirSync(path)
-      dirs[i].size = size
+      if (await fs.lstatSync(currentPath).isDirectory()) {
+        let next = await this.deleteFolderRecursive({path: currentPath}, sizes)
+      }
     }
-    return dirs
+    await fs.rmdirSync(dir.path)
+    return sizes
   }
 }
